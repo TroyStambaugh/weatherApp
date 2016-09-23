67c856ba9c03df5fbb00e89603c1e97350a7e439
TroyStambaugh <troystambaugh@WilliamStambaughMBP.local>
2016-09-23 14:36:25 -0400

    weatherapp

--- weatherApp.js
+++ weatherApp.js
@@ -84,9 +84,25 @@ $(function () {
 
 
 
+//test//
 
+function generateCard(personInfo){
+var template=$("#template").html();
+template=template.replace("");
+template=template.replace("");
+template=template.replace("");
+return template;
 
+function testGenerateCard(){
+    var sampleData={
 
+//fill with info
+    };
+}
+
+}
+var html=generateCard(sampleData);
+$("#cards").append(html);
 
 
   
