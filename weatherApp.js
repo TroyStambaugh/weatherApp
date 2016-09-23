cbf8aad19261739b0e071b8905f1c7d6775a5b6f
TroyStambaugh <troystambaugh@WilliamStambaughMBP.local>
2016-09-22 22:03:42 -0400

    weatherapp

--- weatherApp.js
+++ weatherApp.js
@@ -25,12 +25,31 @@ $(function () {
 
 });
 
-function lookupLatLong_Complete(result) {
-            var latitude = result.results["0"].geometry.location.lat;
-            var longitude = result["0"].geometry.location.lng;
-            console.log("The lat and long is " + latitude + ", " + longitude);
-        }
 
+function lookupLatLong_Complete(result) {
+            var lat = result.results["0"].geometry.location.lat;
+            var long = result.results["0"].geometry.location.lng;
+            console.log("The lat and long is " + lat + ", " + long);
+            
+            var darkSkyUrl = "https://api.darksky.net/forecast/f76dbd347401ed83f364678ffc75ffae/" + lat + "," + long;
+            localWeather(darkSkyUrl);
+        }
+        function localWeather(darkSkyUrl){
+            var weather =  {
+                url: darkSkyUrl,
+                dataType: "jsonp",  
+                success: darksky_complete
+            };
+        $.ajax(weather);
+        }
+    
+        function darksky_complete(weather){
+        
+           console.log("Latitude: " + weather.latitude);
+           console.log("Longitude: " + weather.longitude);
+           console.log("Time zone: " + weather.timezone);
+          console.log("Current Weather: " + weather.currently.summary);
+        }
         function lookupLatLong(city, state, postalCode) {
             // Create the address.
             var address = "";
@@ -43,29 +62,34 @@ function lookupLatLong_Complete(result) {
             else {
                 return; // they didn't give me anything valid, so exit
             }
-
             
-            var googleUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "AIzaSyBZ3-N0emDDcRv06ohzzKn4jF00eev977Y";
-
+            var googleUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyBZ3-N0emDDcRv06ohzzKn4jF00eev977Y";
             var request = {
                 url: googleUrl,
                 success: lookupLatLong_Complete
             };
-
             $.ajax(request);
+            
         }
+// putting google and darksky together
+    
+function lookupWeatherForPostalCode_Click() {
+    var pcode = $("#postalCode").val();
+    lookupLatLong("", "", pcode);
+}
+//  what makes the zip work
+$(function () {
+    $("#lookupWeatherForPostalCode").on("click", lookupWeatherForPostalCode_Click)
+});
 
-        function lookupWeatherForPostalCode_Click() {
-            var pcode = $("#postalCode").val();
-            lookupLatLong("", "", pcode);
-        }
 
-        
 
-        $(function() {
-            $("#lookupWeatherForPostalCode").on("click", lookupWeatherForPostalCode_Click)
-        });
 
+
+
+
+
+  
 
 
 
