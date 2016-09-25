6a400d52308f72fb311d96b6246b1cfc1ecb92e3
TroyStambaugh <troystambaugh@WilliamStambaughMBP.local>
2016-09-25 15:21:39 -0400

    weatherapp

--- weatherApp.js
+++ weatherApp.js
@@ -1,133 +1,106 @@
-  var locationName = " ";
-        var currentTemp = " ";
-        var precip = " ";
-        var max = " ";
-        var min = " ";
-        var currentSum = " ";
+var locationName = " ";
+var currentTemp = " ";
+var precip = " ";
+var max = " ";
+var min = " ";
+var currentSum = " ";
 
-     //pulls lat and lng from google
+//pulls lat and lng from google
 function lookupLatLong_Complete(result) {
-            var lat = result.results[0].geometry.location.lat;
-            var long = result.results[0].geometry.location.lng;
-           
-            locationName = result.results[0].formatted_address;
-           
-            //calls darksky
-            var darkSkyUrl = "https://api.darksky.net/forecast/f76dbd347401ed83f364678ffc75ffae/" + lat + "," + long;
-            localWeather(darkSkyUrl);
-            
-        }
+        var lat = result.results[0].geometry.location.lat;
+        var long = result.results[0].geometry.location.lng;
 
-       
-        function localWeather(darkSkyUrl){
-            var weather =  {
+        locationName = result.results[0].formatted_address;
+
+        //calls darksky
+        var darkSkyUrl = "https://api.darksky.net/forecast/f76dbd347401ed83f364678ffc75ffae/" + lat + "," + long;
+        localWeather(darkSkyUrl);
+
+}
+
+
+function localWeather(darkSkyUrl) {
+        var weather = {
                 url: darkSkyUrl,
-                dataType: "jsonp", 
+                dataType: "jsonp",
                 success: darksky_complete
-            };
+        };
 
         $.ajax(weather);
 
-        }
-    
-        function darksky_complete(weather){
-// Info that is coming from darksky
-           precip = weather.daily.data[1].precipIntensity; 
-           max = weather.daily.data[1].temperatureMax;
-           min = weather.daily.data[1].temperatureMin;
-           currentTemp = weather.currently.temperature;
-           currentSum = weather.currently.summary;
-           
-            generateNewCard();
-        }
-        function lookupLatLong(city, state, postalCode) {
-            var address = "";
-            if (postalCode.length != 0) {
+}
+
+function darksky_complete(weather) {
+        // Info that is coming from darksky
+        precip = weather.daily.data[1].precipIntensity;
+        max = weather.daily.data[1].temperatureMax;
+        min = weather.daily.data[1].temperatureMin;
+        currentTemp = weather.currently.temperature;
+        currentSum = weather.currently.summary;
+
+        generateNewCard();
+}
+function lookupLatLong(city, state, postalCode) {
+        var address = "";
+        if (postalCode.length != 0) {
                 address = postalCode.trim();
-            }
-            else if (city.length != 0 && state != 0) {
+        }
+        else if (city.length != 0 && state != 0) {
                 address = city.trim() + ", " + state;
-            }
-            else {
+        }
+        else {
                 return;
-            }
+        }
 
-            
-            var googleUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyBYlXyp7yQ0n8EYG0lotJPD7sdfKj24PNg";
 
-            var request = {
+        var googleUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyBYlXyp7yQ0n8EYG0lotJPD7sdfKj24PNg";
+
+        var request = {
                 url: googleUrl,
                 success: lookupLatLong_Complete
-            };
+        };
 
-            $.ajax(request);
-        }
-            
-        
-    
-function lookupWeatherForPostalCode_Click() {
-    var pcode = $("#ZIP").val();
-    lookupLatLong("", "", pcode);
+        $.ajax(request);
 }
-function newCard (){
-        var template = 
-            $("#templateCard").html(); 
+
+
+
+function lookupWeatherForPostalCode_Click() {
+        var pcode = $("#ZIP").val();
+        lookupLatLong("", "", pcode);
+}
+function newCard() {
+        var template =
+                $("#templateCard").html();
         //Replace values
-         template = template.replace("@@CITY@@", locationName);
-         template = template.replace("@@TEMP@@", parseInt(currentTemp) + "&degF <br/> <br/> <br/>" + currentSum); 
+        template = template.replace("@@CITY@@", locationName);
+        template = template.replace("@@TEMP@@", parseInt(currentTemp) + "&degF <br/> <br/> <br/>" + currentSum);
         template = template.replace("@@HIGH@@", "High" + "<br/>" + parseInt(max) + "&degF");
         template = template.replace("@@LOW@@", "Low" + "<br/>" + parseInt(min) + "&degF");
         template = template.replace("@@PRECIP@@", "Precip" + "<br/>" + precip + " %");
-        
+
         return template;
-        };
+};
 
-        function generateNewCard() {
-            
-            var card = newCard();
-            $("#cards").append(card);
+function generateNewCard() {
 
-        }
+        var card = newCard();
+        $("#cards").append(card);
+
+}
 
 
 
 
 
 $(function () {
-    $("#lookupWeatherForPostalCode").on("click", lookupWeatherForPostalCode_Click)
-    $(document).on('click', '#remove', function() {
-       $(this).closest('div').remove();
-   });
-  
+        $("#lookupWeatherForPostalCode").on("click", lookupWeatherForPostalCode_Click)
+        $(document).on('click', '#remove', function () {
+                $(this).closest('div').remove();
+        });
+
 });
 
 
-switch (weatherdata.icon){
-    case "clear-day":"url(wimage/sunny.jpeg)";
-          break;
-    case "clear-night":
-            break;
-    case "wind":
-            break;
-    case "snow":
-            break;
-    case "sleet": 
-            break;
-    case "rain":
-            break;
-    case "thunderstorm":
-            break;
-    case "cloudy":
-            break; 
-    case "partly-cloudy-day":
-            break;
-    case "partly-cloudy-night":
-            break;
-    case "fog":
-            break;
-    case "hail":
-            break;
-    case "tornado":
-            break;
-      default:
-}
+
+
